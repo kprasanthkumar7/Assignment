1baacdf435ec80979f14cd4393c61851d99a2165
kprasanthkumar7 <155616381+kprasanthkumar7@users.noreply.github.com>
2024-02-25 23:12:28 +0530

    Update AbstractComponents.java

--- src/main/java/Klaar/AbstractComponents/AbstractComponents.java
+++ src/main/java/Klaar/AbstractComponents/AbstractComponents.java
@@ -24,14 +24,6 @@ public class AbstractComponents {
         PageFactory.initElements(driver,this);
     }
 
-    @FindBy(xpath ="//*[@id='topcartlink']" )
-    WebElement shoppingCart;
-
-//    driver.findElement(By.xpath("//*[text()='Log out']")).click();
-
-
-    @FindBy(xpath = "//*[text()='Log out']")
-    WebElement logOutButton;
 
     public void waitAndCheckElementIsPresent(WebElement element){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
